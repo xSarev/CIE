f3f040fb1b81839e509fcb6ce44e3837673d1812
xSarev <dorin.sarev@icloud.com>
2021-05-15 16:06:29 +0300

    Call decrypt on image

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <thread>
 
 #include "cie.h"
-#include "utils.cpp"
+#include "utils.h"
 
 static void displayImages(int argc, char **argv)
 {
     // check if image path is valid 
     for (unsigned int i = 0; i < argc; i++)
     {
-        Utils::displayImage(argv[i]);
+        CIEUtils::displayImage(argv[i]);
     }   
 }
 
@@ -18,15 +18,36 @@ int main(int argc, char **argv)
 {   
     Magick::InitializeMagick(*argv);
 
-    std::thread thread1(displayImages, argc, argv);
+    // std::thread thread1(displayImages, argc, argv);
 
     std::shared_ptr<CImageEncryption> pImageEncryption = std::make_shared<CImageEncryption>();
+    Magick::Image inputImage;
+
+    try {
+        CIEUtils::checkImageValid(argv[1]);
+        inputImage.read(argv[1]);
+    }
+    catch(std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
     pImageEncryption->generatePrimeNumbers();
     pImageEncryption->computeAlgorithm();
     pImageEncryption->display();
-    // pImageEncryption->encrypt();
-    // pImageEncryption->decrypt(17);
+    pImageEncryption->encrypt(inputImage);
+
+    try {
+        CIEUtils::checkImageValid("encrypted_image.png");
+        inputImage.read("encrypted_image.png");
+    }
+    catch(std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    pImageEncryption->decrypt(inputImage);
 
     // wait thread to finish
-    thread1.join();
+    // thread1.join();
 }
